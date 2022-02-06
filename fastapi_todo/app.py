9d76675c2711940927b293b52aace569be86efba
michael tan <michael.tan@ceridian.com>
2022-02-06 14:43:23 -0500

    add missing requirements

--- fastapi_todo/app.py
+++ fastapi_todo/app.py
@@ -1,9 +1,8 @@
+import uvicorn
 from fastapi import FastAPI
-import unicorn
 
 from fastapi_todo.views.todo_views import todo_router
 from fastapi_todo.views.user_views import user_router
-
 
 app = FastAPI(openapi_url="/openapi.json")
 
@@ -13,4 +12,4 @@ app.include_router(user_router, prefix="/users")
 app.include_router(todo_router, prefix="/todos")
 
 if __name__ == '__main__':
-    unicorn.run(app, host = "0.0.0.0", port = 8080);
+    uvicorn.run(app, host='0.0.0.0', port=8080, log_level='debug');
